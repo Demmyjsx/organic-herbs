22e72bdb36b11b7c73ef9a8d73e3dfd0aa84d417
demola <ademolaabds@gmail.com>
2025-01-13 15:55:51 +0100

    organic herbs commit

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -1,16 +1,16 @@
 export const products = [
   {
-    image: "src/assets/Aswagandha-Extract.jpg",
+    image: "/assets/Aswagandha-Extract.jpg",
     title: "Aswagandha Extract",
     description: "Pure Himalayan Shilajit"
   },
   {
-    image: "src/assets/Curcumin-Dry-Extract.jpg",
+    image: "/assets/Curcumin-Dry-Extract.jpg",
     title: "Curcumin Dry Extract",
     description: "Natural herbal extract"
   },
   {
-    image: "src/assets/Ganoderma-Dry-Extract-11.jpg",
+    image: "/assets/Ganoderma-Dry-Extract-11.jpg",
     title: "Ganoderma Dry Extract",
     description: "Natural herbal extract"
   },
@@ -20,28 +20,28 @@ export const products = [
     description: "Natural herbal extract"
   },
   {
-    image: "src/assets/Garcinia-Combogia-Dry-Extract.jpg",
+    image: "/assets/Garcinia-Combogia-Dry-Extract.jpg",
     title: "Garcinia Combogia Dry Extract",
     description: "Natural herbal extract"
   },
   {
-    image: "src/assets/Shilajit-Dry-Extract.jpg",
+    image: "/assets/Shilajit-Dry-Extract.jpg",
     title: "Shilajit Dry Extract",
     description: "Natural herbal extract"
   },
   {
-    image: "src/assets/Safed-Musli-Dry-Extract.jpg",
+    image: "/assets/Safed-Musli-Dry-Extract.jpg",
     title: "Safed Musli Dry Extract",
     description: "Natural herbal extract"
   },
   {
-    image: "src/assets/Morinda-Extract.jpg",
+    image: "/assets/Morinda-Extract.jpg",
     title: "Morinda Extract / Noni Extract",
     description: "Natural herbal extract"
   },
  
   {
-    image: "src/assets/Spirulina-Powder.jpg",
+    image: "/assets/Spirulina-Powder.jpg",
     title: "Spirulina Powder",
     description: "Natural herbal extract"
   },
