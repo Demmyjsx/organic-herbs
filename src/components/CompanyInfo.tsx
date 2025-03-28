ee9576163c351a91644d0c09e69a784ce944bfe5
Ademola  Abdul <ademolaabdul@Ademolas-MacBook-Pro.local>
2025-03-28 10:15:31 +0100

    changes the email and corrected other spellings

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -8,9 +8,7 @@ export function CompanyInfo() {
           <p>
             <span className="font-medium">Address:</span> FVX9+7GQ, Unnamed Road, East Kombos, Singkil, Manado City, North Sulawesi, Indonesia
           </p>
-        { /* <p>
-            <span className="font-medium">Phone:</span> +62-83872929468
-          </p>*/}
+       
           <p>
             <span className="font-medium">Office time:</span> 8:00am to 7:00pm
           </p>
@@ -18,9 +16,7 @@ export function CompanyInfo() {
             <p>
               <span className="font-medium">Email:</span> info@organicosherbal.com
             </p>
-           {/* <p>
-              <span className="font-medium">Email:</span> sales@organicosherbalfarm.com
-            </p>*/}
+           
           </div>
         </div>
       </div>
